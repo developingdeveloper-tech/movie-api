2695baab2bb1ee48fda18da34cf859dd9efe3ff6
5abhisheksaxena <5abhisheksaxena@gmail.com>
2021-03-14 16:40:24 +0530

    Add timestamp to error messages

--- src/main/kotlin/tech/developingdeveloper/movieapi/utils/exceptions/ApiError.kt
+++ src/main/kotlin/tech/developingdeveloper/movieapi/utils/exceptions/ApiError.kt
@@ -1,12 +1,14 @@
 package tech.developingdeveloper.movieapi.utils.exceptions
 
 import org.springframework.http.HttpStatus
+import java.time.LocalDateTime
 
 
 data class ApiError(
     private val _message: String?,
     val status: HttpStatus = HttpStatus.BAD_REQUEST,
-    val code: Int = status.value()
+    val code: Int = status.value(),
+    val timestamp: LocalDateTime = LocalDateTime.now()
 ){
     val message: String
         get() = _message ?: "Something went wrong"
